fd2732c8437fa49a457eb114ab2f52192e57b20f
Joe Cavanagh <joe.cavanagh@corvisa.com>
2013-07-23 15:57:57 -0500

    Vibration pattern and some comments

--- src/com/jlcavanagh/morsevibes/receivers/TextReceiver.java
+++ src/com/jlcavanagh/morsevibes/receivers/TextReceiver.java
@@ -96,25 +96,60 @@ public class TextReceiver extends BroadcastReceiver {
 
                 //Pull the contact by phone number
                 if(msgFrom != null) {
+                    //Assemble the filter URI
                     Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(msgFrom));
                     String name = "";
 
+                    //Query the contacts DB and grab a cursor
                     ContentResolver contentResolver = context.getContentResolver();
                     Cursor contactLookup = contentResolver.query(uri, new String[] {BaseColumns._ID,
                             ContactsContract.PhoneLookup.DISPLAY_NAME }, null, null, null);
 
                     try {
+                        //If we found anything, pull the name
                         if (contactLookup != null && contactLookup.getCount() > 0) {
                             contactLookup.moveToNext();
                             name = contactLookup.getString(contactLookup.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
                             //String contactId = contactLookup.getString(contactLookup.getColumnIndex(BaseColumns._ID));
                         } else {
+                            //FIXME: Fallback of some sort?  Configurable?
                             Log.d(TAG, "Could not find contact for phone: " + msgFrom);
                         }
                     } finally {
                         if (contactLookup != null) {
                             contactLookup.close();
                         }
+                    }
+
+                    //Vibe first letter of name
+                    if(name != null) {
+                        //Get the morse code for the first character
+                        Character firstChar = name.toUpperCase().charAt(0);
+                        String code = morse.get(firstChar);
+
+                        if(code != null) {
+                            //If we got a code, assemble the vibration pattern
+                            long[] vibePattern = new long[code.length() * 2];
+
+                            //Vibration pattern is [time off, time on, time off, ...]
+                            for(int x = 0; x < code.length(); x++) {
+                                vibePattern[x * 2] = PAUSE;
+
+                                //If zero, vibe short, else vibe long
+                                if(code.charAt(x) == '0') {
+                                    vibePattern[(x * 2) + 1] = SHORT;
+                                } else {
+                                    vibePattern[(x * 2) + 1] = LONG;
+                                }
+                            }
+
+                            //Dat vibe
+                            v.vibrate(vibePattern, -1);
+                        } else {
+                            Log.d(TAG, "No morse mapping for char: " + firstChar);
+                        }
+                    } else {
+                        Log.d(TAG, "Could not find name for contact with phone: " + msgFrom);
                     }
                 }
             } catch(Exception e){
