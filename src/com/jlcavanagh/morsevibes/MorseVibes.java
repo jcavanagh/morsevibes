9c333a934c3a3bd4f2d30c8639909d6a5d7932b8
Joe Cavanagh <joe.cavanagh@corvisa.com>
2013-07-23 15:39:10 -0500

    Barebones project

---
+++ src/com/jlcavanagh/morsevibes/MorseVibes.java
@@ -0,0 +1,22 @@
+package com.jlcavanagh.morsevibes;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MorseVibes extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_morse_vibes);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.morse_vibes, menu);
+		return true;
+	}
+
+}
